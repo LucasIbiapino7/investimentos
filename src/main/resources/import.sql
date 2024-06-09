e42d254b1ff835041b1dd6431f02ada6a321c532
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2024-06-09 18:18:16 -0300

    Seeding para testes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,13 @@ INSERT INTO tb_user(first_name, last_name, birth_date, email, password) VALUES (
 INSERT INTO tb_role(authority) VALUES ('ROLE_CLIENT')
 
 INSERT INTO tb_user_role(user_id, role_id) VALUES (1, 1)
+
+INSERT INTO tb_account(user_id, name, description, balance, portfolio_number) VALUES (1, 'minha conta', 'Conta do Lucas', 1000.0, 0);
+
+INSERT INTO tb_portfolio(description, total_value, account_id) VALUES ('Ativos', 0.0, 1);
+
+INSERT INTO tb_stock(name, long_name) VALUES ('AMZO34', 'Amazon.inc')
+INSERT INTO tb_stock(name, long_name) VALUES ('PETR4', 'Petrobrás')
+
+INSERT INTO tb_portfolio_stock(stock_id, portfolio_id, price, quantity, value_purchased) VALUES ('AMZO34', 1, 40.0, 5, 200.0);
+INSERT INTO tb_portfolio_stock(stock_id, portfolio_id, price, quantity, value_purchased) VALUES ('PETR4', 1, 10.0, 10, 100.0);
